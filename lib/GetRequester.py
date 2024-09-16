d81dc64fc913ca37a2d102ae9099cae1206d39ff
Abdiaziz82 <abdiazizhared64@gmail.com>
2024-09-16 10:17:19 +0300

    complete_lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,10 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+      response = requests.get(self.url)
+      return response.content
 
     def load_json(self):
-        pass
+        # Get the raw response body and convert it directly to JSON
+        json_data = requests.get(self.url).json()
+        return json_data  # This will be a Python dictionary or list
